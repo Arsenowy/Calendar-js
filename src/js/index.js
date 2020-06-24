b48a3cf003bf91333e1cf3a72c34b0602c5a26d6
Mikołaj Krawczyk <mkkrawczyk00@gmail.com>
2020-06-24 22:52:47 +0200

    Add listeners to all fields and notes (js)

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,21 @@ import "regenerator-runtime/runtime";
 // local
 import "../sass/style.scss";
 
-
 // import * as smt from './';
 
 
+
+const fields = document.querySelectorAll('.grid__hours--full');
+
+// Add listener to all fields
+Array.from(fields).forEach(cur => {
+    cur.addEventListener('click', field => {
+        //delete this later
+        field.target.classList.toggle('red');
+
+        //1. Show window with forms //ui
+        //2. get data and save it in object  //controller
+        //3. Hide window //ui
+        //4. Change field's color and text inside
+    }
+)})
